c85c7ab7a65b60587866a854bc67a2f487682656
gangyuji <gangyoyul9@gmail.com>
2023-04-18 21:34:14 +0900

    for range() indexing practice

--- for_pra.py
+++ for_pra.py
@@ -11,5 +11,19 @@ my_list = ['a', 'b', 'c', 'd', 'e', 'f']
 # for g in range(100, 89, -1):
 #     print(g) 
 
-for g in range(10, 0, -3):
-    print(g) 
+# for g in range(10, 0, -3):
+#     print(g) 
+
+
+
+# for g in range(0, 5, 1):
+#     print(my_list[g])
+
+# for g in range(2, 6):
+#     print(my_list[g])
+
+# for g in range(3, 0, -1):
+#     print(my_list[g])
+
+for g in range(0, 6, 2):
+    print(my_list[g])
