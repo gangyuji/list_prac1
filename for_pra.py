34628463c0ac4caefeb8aaf56caed96ac03fbf3c
gangyuji <gangyoyul9@gmail.com>
2023-04-18 20:59:59 +0900

    for range() practice

---
+++ for_pra.py
@@ -0,0 +1,15 @@
+import time
+
+my_list = ['a', 'b', 'c', 'd', 'e', 'f']
+
+# for g in range(1, 11, 1):
+#     print(g) 
+
+# for g in range(0, 11, 2):
+#     print(g) 
+
+# for g in range(100, 89, -1):
+#     print(g) 
+
+for g in range(10, 0, -3):
+    print(g) 
--- list_pra.py
+++ list_pra.py
@@ -3,10 +3,10 @@
 # new_list = my_list[1:3]
 # print(new_list)
 
-my_list = ['a', 'b', 'c', 'd', 'e', 'f']
+# my_list = ['a', 'b', 'c', 'd', 'e', 'f']
 
 
-print(my_list[0:5])
-print(my_list[2:])
-print(my_list[3:0:-1])
-print(my_list[::2])
+# print(my_list[0:5])
+# print(my_list[2:])
+# print(my_list[3:0:-1])
+# print(my_list[::2])
