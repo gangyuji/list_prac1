1778d4f55ef20d0815d017deadcebc8a24802b65
gangyuji <gangyoyul9@gmail.com>
2023-04-21 20:39:33 +0900

    space & , add

--- print_pt.py
+++ print_pt.py
@@ -3,6 +3,11 @@ import datetime
 my_name = input()
 today = datetime.datetime.now().date()
 
-print(f"{my_name}님 안녕하세요 오늘은 {today} 입니다.")
+t = today.strftime("%y년%m월%d일")
+
+print(f"{my_name}님 안녕하세요 오늘은 {t} 입니다.")
+
+now = datetime.now()
 
 
+# print(f"{my_name}님 안녕하세요 오늘은 {now.year}년 {now.month}월 {now.day}일 입니다.")
